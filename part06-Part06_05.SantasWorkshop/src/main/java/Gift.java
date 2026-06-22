b260e554b076db10ebfd064e4bab2335c4f12f24
DumitriuMihnea05 <mihnea4iustin@gmail.com>
2026-06-22 08:33:44 -0400

    Add files via upload

---
+++ part06-Part06_05.SantasWorkshop/src/main/java/Gift.java
@@ -0,0 +1,22 @@
+public class Gift {
+    private String name = "";
+    private int weight = 0 ; 
+    
+    public  Gift(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+    
+    public String getName() {
+        if(name != null ) {
+            return this.name;
+        }
+        return "";
+    }
+    
+    public int getWeight() {
+        return this.weight;
+    }
+    
+    
+    }

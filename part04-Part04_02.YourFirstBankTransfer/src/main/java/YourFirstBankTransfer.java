6a7177f0fa5870c6b15c00252168062980fcd237
DumitriuMihnea05 <mihnea4iustin@gmail.com>
2026-06-22 08:23:56 -0400

    Add files via upload

---
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -0,0 +1,14 @@
+
+public class YourFirstBankTransfer {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // write your program here
+        Account Matthews_account = new Account("Matthews account" ,1000 );
+        Account myAccount = new Account("My account", 0);
+        Matthews_account.withdrawal(100);
+        myAccount.deposit(100);
+        System.out.println(Matthews_account);
+        System.out.println(myAccount);
+    }
+}
---
+++ part04-Part04_04.Room/src/main/java/Room.java
@@ -0,0 +1,9 @@
+public class Room {
+    private String code;
+    private  int seats;
+    public room (String classCode , int numberOfSeats){
+        this.classCode = code;
+        this numberOfSeats = seats;
+    }
+
+}
